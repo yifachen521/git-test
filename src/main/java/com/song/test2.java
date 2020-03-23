4891d2d9a34c2b13728e8ce06a380960968ba0ac
炜哥哥 <https://gitee.com/studayOnQF/FengMiNet.git>
2020-03-23 21:07:30 +0800

    test2

---
+++ src/main/java/com/song/test2.java
@@ -0,0 +1,12 @@
+package com.song;
+
+/**
+ * @author 炜哥哥
+ * @date 2020/3/23 20:30
+ */
+public class test2 {
+    public static void main(String[] args) {
+
+        System.out.println("hahah2");
+    }
+}
