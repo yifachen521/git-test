1ebc85e18a2de8fc1ddec008c5f4c2d1997d6415
炜哥哥 <https://gitee.com/studayOnQF/FengMiNet.git>
2020-03-23 20:32:18 +0800

    第一次提交

---
+++ src/main/java/com/song/test1.java
@@ -0,0 +1,14 @@
+package com.song;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author 炜哥哥
+ * @date 2020/3/23 20:30
+ */
+public class test1 {
+    public static void main(String[] args) {
+
+        System.out.println("第一次提交到git");
+    }
+}
